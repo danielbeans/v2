05f227464747f68c5a2c4b2862d450f69214d91f
Daniel Williams <danielfrwilliams@gmail.com>
2022-01-02 20:34:39 -0500

    Create Gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.danielfwilliams.com",
+    title: "v2",
+  },
+  plugins: [`gatsby-plugin-postcss`],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "dark-blue": "#172231",
+      "light-blue": "#C1D6F1",
+      blue: "#33A9FE",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
